ad44c698560489db272921194e0f84bf2e7bc3c3
Glenn Eggleton <geggleto@gmail.com>
2016-02-16 10:21:39 -0500

    Update for 2.1 Ref #16

--- src/PhpRenderer.php
+++ src/PhpRenderer.php
@@ -11,7 +11,8 @@ namespace Slim\Views;
 use Psr\Http\Message\ResponseInterface;
 
 /**
- * Php View
+ * Class PhpRenderer
+ * @package Slim\Views
  *
  * Render PHP view scripts into a PSR-7 Response object
  */
@@ -39,8 +40,8 @@ class PhpRenderer
      *
      * throws RuntimeException if $templatePath . $template does not exist
      *
-     * @param \ResponseInterface $response
-     * @param                    $template
+     * @param ResponseInterface $response
+     * @param string             $template
      * @param array              $data
      *
      * @return ResponseInterface
@@ -50,6 +51,29 @@ class PhpRenderer
      */
     public function render(ResponseInterface $response, $template, array $data = [])
     {
+        $output = $this->fetch($template, $data);
+
+        $response->getBody()->write($output);
+        
+        return $response;
+    }
+
+    /**
+     * Renders a template and returns the result as a string
+     *
+     * cannot contain template as a key
+     *
+     * throws RuntimeException if $templatePath . $template does not exist
+     *
+     * @param $template
+     * @param array $data
+     *
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
+     */
+    public function fetch($template, array $data = []) {
         if (isset($data['template'])) {
             throw new \InvalidArgumentException("Duplicate template key found");
         }
@@ -58,17 +82,19 @@ class PhpRenderer
             throw new \RuntimeException("View cannot render `$template` because the template does not exist");
         }
 
-        $render = function ($template, $data) {
-            extract($data);
-            include $template;
-        };
-
         ob_start();
-        $render($this->templatePath . $template, $data);
-        $output = ob_get_clean(); 
+        $this->protectedIncludeScope($this->templatePath . $template, $data);
+        $output = ob_get_clean();
 
-        $response->getBody()->write($output);
-        
-        return $response;
+        return $output;
+    }
+
+    /**
+     * @param string $template
+     * @param array $data
+     */
+    protected function protectedIncludeScope ($template, array $data) {
+        extract($data);
+        include $template;
     }
 }
